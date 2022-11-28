b82fd0a8082f7a5bb2cb644aefd28bda03fe6aef
marcelrm11 <marcelrm11@gmail.com>
2022-11-28 15:57:55 +0000

    get, post, put, delete in api

--- src/js/component/todo-list.jsx
+++ src/js/component/todo-list.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function TodoList() {
 
@@ -6,12 +6,23 @@ function TodoList() {
     const [taskList, setTaskList] = useState(new Set());
     const [hasTask, setHasTask] = useState(false);
 
+    useEffect(() => {
+        console.log("useEffect createUser!");
+        createUser("marcelrm11"); // creates the user and the todo list in the server
+    }, []); // run an effect and clean it up only once
+
+    useEffect(() => {
+        console.log("useEffect getTodos!");
+        getUserTodoList("marcelrm11");
+    })
+
     function handleEnter(key) {
         if (key == "Enter") {
             setHasTask(taskList.has(newTask))
             let newTaskList = taskList.add(newTask);
             setTaskList(newTaskList);
             setNewTask("");
+            updateApiList("marcelrm11", apiListFromSet(newTaskList));
         }
     }
 
@@ -20,7 +31,56 @@ function TodoList() {
         let newTaskList = new Set(taskList);
         newTaskList.delete(toBeDeleted);
         setTaskList(newTaskList);
+        updateApiList("marcelrm11", apiListFromSet(newTaskList));
     }
+
+    function apiListFromSet(mySet) {
+        let apiList = [];
+        for (let task of mySet) {
+            const taskObj = { label: task, done: false};
+            apiList.push(taskObj);
+        }
+        return apiList;
+    }
+
+    function createUser(username) {
+        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
+            method: "POST",
+            body: "[]",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => {
+            console.log(response.status);
+            return response.json();
+        })
+        .then(data => {
+            console.log(data.msg);
+        })
+        .catch(error => console.log(error));
+    }
+
+    function getUserTodoList(username) {
+        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }
+
+    function updateApiList(username, newList) {
+        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
+            method: "PUT",
+            body: JSON.stringify(newList),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    } 
 
     return (
         <div className="container d-flex flex-column align-items-center">
