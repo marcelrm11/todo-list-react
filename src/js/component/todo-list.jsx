c4db6f3a3ed3448f6cbe1da65caeb03dd81c130f
marcelrm11 <marcelrm11@gmail.com>
2022-11-29 11:36:03 +0000

    composing reusable components

--- src/js/component/todo-list.jsx
+++ src/js/component/todo-list.jsx
@@ -1,107 +1,141 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import CustomInput from "./custom-input.jsx";
+import SetList from "./set-list.jsx";
 
 function TodoList() {
 
     const [newTask, setNewTask] = useState("");
     const [taskList, setTaskList] = useState(new Set());
     const [hasTask, setHasTask] = useState(false);
+    const [username, setUsername] = useState("");
+    const [myUsersList, setUsersList] = useState(new Set());
 
-    useEffect(() => {
-        console.log("useEffect createUser!");
-        createUser("marcelrm11"); // creates the user and the todo list in the server
-    }, []); // run an effect and clean it up only once
-
-    useEffect(() => {
-        console.log("useEffect getTodos!");
-        getUserTodoList("marcelrm11");
-    })
+    function handleUsername(key) {
+        if (key == "Enter"){
+            createUser(username); // this creates (POST) and GETs the list
+        }
+    }
 
     function handleEnter(key) {
         if (key == "Enter") {
-            setHasTask(taskList.has(newTask))
-            let newTaskList = taskList.add(newTask);
-            setTaskList(newTaskList);
+            let exists;
+            for (const taskObj of taskList) {
+                if (taskObj.label === newTask) {
+                    exists = true;
+                    break;
+                }
+            }
+            let newTaskObj = {label: newTask, done: false}
+            setHasTask(exists);
+            if (!exists) {
+                let newTaskList = taskList.add(newTaskObj);
+                setTaskList(newTaskList);
+                updateApiList(username, newTaskList);
+            }
             setNewTask("");
-            updateApiList("marcelrm11", apiListFromSet(newTaskList));
         }
     }
 
     function deleteTask(e) {
-        let toBeDeleted = e.target.parentElement.textContent;
+        let taskLabel = e.target.parentElement.textContent;
         let newTaskList = new Set(taskList);
-        newTaskList.delete(toBeDeleted);
-        setTaskList(newTaskList);
-        updateApiList("marcelrm11", apiListFromSet(newTaskList));
-    }
-
-    function apiListFromSet(mySet) {
-        let apiList = [];
-        for (let task of mySet) {
-            const taskObj = { label: task, done: false};
-            apiList.push(taskObj);
+        for (const taskObj of newTaskList) {
+            if (taskObj.label === taskLabel) {
+                newTaskList.delete(taskObj);
+                break;
+            }
         }
-        return apiList;
+        if (newTaskList.size === 0) {
+            deleteUser(username);
+        } else {
+            updateApiList(username, newTaskList);
+        }
+        setTaskList(newTaskList);
     }
 
-    function createUser(username) {
-        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
+    async function createUser(username) {
+        let response = await fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
             method: "POST",
             body: "[]",
             headers: {
                 "Content-Type": "application/json"
             }
         })
-        .then(response => {
-            console.log(response.status);
-            return response.json();
-        })
-        .then(data => {
-            console.log(data.msg);
-        })
-        .catch(error => console.log(error));
+        let data = await response.json()
+        console.log(data);
+        getUserTodoList(username);
+        // .catch(error => console.log(error));
     }
 
-    function getUserTodoList(username) {
-        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
+    async function getUserTodoList(username) {
+        let response = await fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json"
             }
         })
-        .then(response => response.json())
-        .then(data => console.log(data))
+        let data = await response.json();
+        console.log(data)
+        let myTaskList = new Set();
+        for (const task of data) {
+            myTaskList.add(task);
+        }
+        setTaskList(myTaskList);
     }
 
     function updateApiList(username, newList) {
         fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
             method: "PUT",
-            body: JSON.stringify(newList),
+            body: JSON.stringify([...newList]),
             headers: {
                 "Content-Type": "application/json"
             }
         })
-    } 
+        .then(response => response.json())
+        .then(data => console.log(data.result))
+        .catch(error => console.log(error))
+    }
+
+    function deleteUser(username) {
+        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    }
 
     return (
         <div className="container d-flex flex-column align-items-center">
             <h1>MY TODO LIST</h1>
-            <input type="text" placeholder="add new task"
+
+            <CustomInput 
+                type={"text"}
+                placeholder={"username"}
+                value={username}
+                onChange={setUsername}
+                onKeyDown={handleUsername}
+            />
+
+            <h4>Welcome {username} to your list!</h4>
+
+            <CustomInput
+                type={"text"}
+                placeholder={"add new task"}
                 value={newTask}
-                onChange={(e) => setNewTask(e.target.value)}
-                onKeyDown={(e) => handleEnter(e.key)}
-            />{hasTask ? <span>Already in the list!</span> : ""}
+                onChange={setNewTask}
+                onKeyDown={handleEnter}
+            />
+            
+            {hasTask ? <span>Already in the list!</span> : ""}
+
             <ul>
-                {[...taskList].map((task, index) => {
-                    return (
-                        <li className="task" key={index}>
-                            {task} 
-                            <i className="fa-solid fa-trash-can delete"
-                            onClick={(e) => deleteTask(e)}
-                            ></i>
-                        </li>
-                    )
-                })}
+                <SetList 
+                    set={taskList} 
+                    itemAction={deleteTask}
+                />
             </ul>
+
             <p>{taskList.size}{' '}{taskList.size === 1 ? "task" : "tasks"} in the list.</p>
         </div>
     )
